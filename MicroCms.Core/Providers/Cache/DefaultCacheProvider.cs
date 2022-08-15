59313259ca2d4f7a9a8b01a0937897528718e52a
manas_nayak <manas_nayak@vpn.ship.gov.sg>
2022-08-15 22:46:37 +0800

    Add Cache Provider

---
+++ MicroCms.Core/Providers/Cache/DefaultCacheProvider.cs
@@ -0,0 +1,42 @@
+﻿namespace MicroCms.Core.Providers.Cache
+{
+    public class DefaultCacheProvider : ICacheProvider
+    {
+        static Dictionary<string, object> _cache = new Dictionary<string, object>();
+        public void Clear()
+        {
+            _cache.Clear();
+        }
+
+        public T? Get<T>(string key)
+        {
+            return _cache.ContainsKey(key) ? (T)_cache[key] : default;
+        }
+
+        public T? GetOrSet<T>(string key, Func<string, T> valFunction)
+        {
+            var value = Get<T>(key);
+            if (null == value)
+                Set<T>(key, valFunction(key));
+            return Get<T>(key);
+        }
+
+        public T? Remove<T>(string key)
+        {
+            var value = Get<T>(key);
+            if (null != value)
+                _cache.Remove(key);
+            return value;
+        }
+
+        public void Set<T>(string key, T value)
+        {
+            if (null == value)
+                return;
+            if (_cache.ContainsKey(key))
+                _cache[key] = value;
+            else
+                _cache.Add(key, value);
+        }
+    }
+}
---
+++ MicroCms.Core/ServiceExtension.cs
@@ -0,0 +1,34 @@
+﻿using MicroCms.Core.Providers.Cache;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MicroCms.Core
+{
+    public static class ServiceExtension
+    {
+        public static void AddMicroCms(this IServiceCollection serviceProvider, IConfiguration configuration)
+        {
+            var option = MicroCmsConfigurationOption.Default;
+            serviceProvider.AddSingleton(typeof(ICacheProvider), Type.GetType(option.CacheProvider));
+        }
+        
+    }
+    public class MicroCmsConfigurationOption
+    {
+        public bool CacheEnabled { get; set; }
+        public string CacheProvider { get; set; }
+        public string DbProvider { get; set; }
+
+
+        public static MicroCmsConfigurationOption Default { get; set; }
+        static MicroCmsConfigurationOption()
+        {
+            Default = new MicroCmsConfigurationOption() { 
+                CacheEnabled = true ,
+                CacheProvider = "MicroCms.Core.Providers.Cache.DefaultCacheProvider,MicroCms.Core"
+            };
+        }
+
+    }
+}
